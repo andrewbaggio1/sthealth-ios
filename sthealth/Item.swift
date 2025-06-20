7bfcfde856df70acfa22c10673429c3af8e901ff
Andrew Baggio <andrewbaggio1@gmail.com>
2025-06-20 13:06:05 -0500

    Initial Commit

---
+++ sthealth/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  sthealth
+//
+//  Created by Andrew Baggio on 6/20/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ sthealthTests/sthealthTests.swift
@@ -0,0 +1,17 @@
+//
+//  sthealthTests.swift
+//  sthealthTests
+//
+//  Created by Andrew Baggio on 6/20/25.
+//
+
+import Testing
+@testable import sthealth
+
+struct sthealthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
